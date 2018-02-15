8bb9db0633699e475c45b9cfed4acc5484ebea01
jfcamel <fujita.jun@gmail.com>
2018-02-16 01:19:58 +0900

    Skeleton with minimum impl

---
+++ server.js
@@ -0,0 +1,25 @@
+
+"use strict";
+
+const express = require("express");
+
+const hostname = "0.0.0.0"
+const port = 8080;
+
+module.exports = class RelayServer {
+
+  constructor(datasource) {
+    this.server = express();
+    this.datasource = datasource;
+  }
+
+  start() {
+    this.server.get("/", (req, res) => {
+      res.send("no implemention");
+    });
+
+    this.server.listen(port, hostname);
+    console.log(`Server running at http://${hostname}:${port}`);
+  }
+
+};
